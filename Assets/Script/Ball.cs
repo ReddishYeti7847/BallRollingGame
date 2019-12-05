46365e10ffddd0d347630c14810428c2e99233b0
ReddishYeti7847 <noriyasu200081@gmail.com>
2019-12-05 17:07:28 +0900

    カメラとボールの親子関係をなくし、ボールとカメラの不自然な挙動を修正した。

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
+    //スピード
     public int floorspeed;
     public int ballspeed;
+    //カメラオブジェクト
+    public GameObject mainCamera;
 
     //private Rigidbody BallRigid;//PlayerオブジェクトのRigidbobyを保管する
     //public float JumpSpeed;
-    private Vector3 point;
+    private Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +23,14 @@ public class Ball : MonoBehaviour
         Debug.Log("floorspeed" + floorspeed);
         Debug.Log("ballspeed" + ballspeed);
 
+        mainCamera = GameObject.Find("Main Camera");
+
         //BallRigid = Ball.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        point = this.transform.position;
-
-        if (point.y < -10)
-        {
-            GameOver();
-        }
-
         //ボールを前に動かす
         transform.Translate(0, 0, floorspeed * Time.deltaTime);
 
@@ -47,11 +45,22 @@ public class Ball : MonoBehaviour
             transform.Translate(ballspeed * Time.deltaTime, 0, 0);
         }
 
-    /* if (Input.GetKeyDown(KeyCode.Return))
-    {
-        //上にジャンプする。
-        BallRigid.AddForce(transform.up * JumpSpeed);
-    } */
+        /* if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //上にジャンプする。
+            BallRigid.AddForce(transform.up * JumpSpeed);
+        } */
+
+        pos = this.transform.position;
+
+        //落下判定
+        if (pos.y < -10)
+        {
+            GameOver();
+        }
+
+        //カメラを追従させる
+        mainCamera.transform.position = new Vector3(pos.x, pos.y + 3, pos.z - 5);
     }
 
     //ボールが一定以上下に落ちたらゲームオーバーにする
